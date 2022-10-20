687e2990160acd652f98b3764ed48bef897bcad5
KaiserDPP <dpalanca20@gmail.com>
2022-10-20 10:44:18 +0200

    subida jtojson

---
+++ src/main/java/io/codejournal/maven/java2jsonschema/Employee.java
@@ -0,0 +1,21 @@
+package io.codejournal.maven.java2jsonschema;
+
+public class Employee {
+	private int id;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
